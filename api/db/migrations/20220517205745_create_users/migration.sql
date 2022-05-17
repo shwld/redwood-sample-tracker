db7516741df4e921f29fca35a814bba66a7f3b10
shwld <shu.account@outlook.com>
2022-05-18 05:58:58 +0900

    yarn rw prisma migrate dev

---
+++ api/db/migrations/20220517205745_create_users/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" DATETIME
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
